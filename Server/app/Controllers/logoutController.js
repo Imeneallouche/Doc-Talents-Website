a6852f5e20e4f7663a796cfcc1df73e8bbd9f061
Imène ALLOUCJE <https://github.com/Imeneallouche>
2023-03-12 15:06:10 +0100

    controllers of login/register/logout/loggedin have been initialized version1.0

---
+++ Server/app/Controllers/logoutController.js
@@ -0,0 +1,12 @@
+const db = require("../../DB/db_config");
+const bcrypt = require("bcryptjs");
+
+//exports.logout = (req, res) => {
+
+const logout = async (req, res) => {
+  res.cookie("userSave", "logout", {
+    expires: new Date(Date.now() + 2 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).redirect("/");
+};
