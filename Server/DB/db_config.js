a561962f69dcfa071d0d74d626569d6d8e2ae46e
Imène ALLOUCJE <https://github.com/Imeneallouche>
2023-03-28 16:10:50 +0100

    new connection has been settled

--- Server/DB/db_config.js
+++ Server/DB/db_config.js
@@ -12,12 +12,11 @@ const connection = mysql.createConnection({
 });
 
 /*
-  host: "sql12.freesqldatabase.com",
-  user: "sql12607428",
-  password: "KhpIlGsAwV",
-  database: "sql12607428",
+  host: "bi8rqfiqnirh9lxoqlvx-mysql.services.clever-cloud.com",
+  user: "us8pi6zpi1yugtqt",
+  password: "RMueTG5ErjBW3lCO21ix",
+  database: "bi8rqfiqnirh9lxoqlvx",
   port: 3306,
-  
 */
 
 connection.connect((error) => {
@@ -25,9 +24,10 @@ connection.connect((error) => {
   console.log("Database connected successfully");
 });
 
+/*
 const Id_Doctorant = 210021;
 const soutenu = 0;
 const date_soutenance = "21-09-2018";
 const numPvSoutenance = 2018;
-
+*/
 module.exports = connection;
