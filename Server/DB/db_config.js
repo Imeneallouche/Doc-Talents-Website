003c7ea5f8b1063e0167be9ae06e872e8ba5b5cc
Imène ALLOUCJE <https://github.com/Imeneallouche>
2023-04-16 22:17:47 +0100

    data has been geenrated

--- Server/DB/db_config.js
+++ Server/DB/db_config.js
@@ -3,14 +3,21 @@ const env = require("dotenv").config();
 const cookie = require("cookie-parser");
 
 const connection = mysql.createConnection({
+  host: "bi8rqfiqnirh9lxoqlvx-mysql.services.clever-cloud.com",
+  user: "us8pi6zpi1yugtqt",
+  password: "RMueTG5ErjBW3lCO21ix",
+  database: "bi8rqfiqnirh9lxoqlvx",
+  port: 3306,
+});
+
+/*
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
   port: process.env.DATABASE_PORT,
-});
 
-/*
+  
   host: "bi8rqfiqnirh9lxoqlvx-mysql.services.clever-cloud.com",
   user: "us8pi6zpi1yugtqt",
   password: "RMueTG5ErjBW3lCO21ix",
