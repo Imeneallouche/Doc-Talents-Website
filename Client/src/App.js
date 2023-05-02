f867c35725b7a9221cd8697f4146dcfef310ec78
Lenovo <li_louni@esi.dz>
2023-05-02 18:45:58 +0100

    Authentification added

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,48 +1,41 @@
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch , Redirect} from "react-router-dom";
 import "./App.css";
 
-import Inscription from "./pages/Inscription";
+// import Inscription from "./pages/Inscription";
 import Doctorant from "./pages/Doctorant";
 import Acceuil from './pages/Acceuil';
 import Connexion from "./pages/Connexion";
 import Popuppage from "./pages/Popuppage";
 import Update from "./pages/Update";
-import PVs from "./pages/PVs";
-import Users from "./pages/Users";
-import Profile from "./pages/Profile";
-import Editprofile from "./pages/Editprofile";
-import Dashboard from "./pages/Dashboard";
-import Statistics from "./pages/Statistics";
-import Seminaire from "./pages/Seminaire";
-import InscriptionStep1 from "./pages/InscriptionStep1";
-import InscriptionStep2 from "./pages/InscriptionStep2";
-import InscriptionStep3 from "./pages/InscriptionStep3";
-import ConfirmEndacrant from "./pages/ConfirmEndacrant";
-import SeminaireLirePlus from "./pages/SeminaireLirePlus";
+import PrivateRoute from "./components/ProtectedRoute/PrivateRoute"
+import ConnexionNotSuccessfull from "./pages/ConnexionNotSuccessfull";
+// import PVs from "./pages/PVs";
+// import Users from "./pages/Users";
+// import Profile from "./pages/Profile";
+// import Editprofile from "./pages/Editprofile";
+// import Dashboard from "./pages/Dashboard";
+// import Statistics from "./pages/Statistics";
+// import Seminaire from "./pages/Seminaire";
+// import InscriptionStep1 from "./pages/InscriptionStep1";
+// import InscriptionStep2 from "./pages/InscriptionStep2";
+// import InscriptionStep3 from "./pages/InscriptionStep3";
+// import ConfirmEndacrant from "./pages/ConfirmEndacrant";
+// import SeminaireLirePlus from "./pages/SeminaireLirePlus";
 
 
 
-export default function App() {
+export default function App(){
   return (
     <div className="App w-screen h-screen">
       <Router>
         <Switch>
-          <Route path="/" exact>
-          <Acceuil/>    
-          </Route>
-          <Route path="/Connexion" exact>
-            <Connexion />
-          </Route>
-          <Route path="/Popuppage" exact>
-            <Popuppage/>
-          </Route>
-          <Route path="/Doctorant" exact>
-            <Doctorant />
-          </Route>
-          <Route path="/Update" exact>
-            <Update />
-          </Route>
-          <Route path="/Dashboard" exact>
+          <Route path="/" exact><Acceuil/></Route>
+          <Route path="/Connexion" exact><Connexion /></Route>
+          <PrivateRoute path="/ConnexionNotSuccessfull" component={ConnexionNotSuccessfull} />
+          <PrivateRoute path="/Popuppage" component={Popuppage} />
+          <PrivateRoute path="/Update" component={Update} />
+          <PrivateRoute path="/Doctorant" component={Doctorant} />
+          {/* <Route path="/Dashboard" exact>
             <Dashboard />
           </Route>
           <Route path="/Inscription/Step1">
@@ -79,8 +72,13 @@ export default function App() {
             <Seminaire />
           </Route>
           <Route path="/SeminaireLirePlus/:title" exact component={SeminaireLirePlus} /> 
-        </Switch>
+        */}</Switch> 
       </Router>
     </div>
   );
 }
+
+
+
+
+
--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const connection = require("./DB/db_config");
-const bodyParser = require('body-parser'); 
-const encoder = bodyParser.urlencoded(); 
 require("dotenv").config();
 const dotenv = require("dotenv");
 dotenv.config();
@@ -58,6 +56,9 @@ app.get("/", (req, res) => {
 */
 
 app.get("/Doctorant", (req, res) => {
+  //authenticate method: get(jwt token) validate(jwt token)
+  //Redirection(if not valid) 
+  //Cookies 
   const sql = `SELECT * FROM Doctorant ORDER BY nom, prenom ASC`;
   connection.query(sql, (error, results) => {
     if (error) {
@@ -75,7 +76,7 @@ app.get("/Doctorant", (req, res) => {
 
 
 
-*/
+// */
 
 app.get("/Update", (req, res) => {
   const sql = `SELECT * FROM Doctorant WHERE radie IS NULL AND soutenu IS NULL ORDER BY nom, prenom ASC`;
@@ -87,127 +88,52 @@ app.get("/Update", (req, res) => {
   });
 });
 
-app.get("/DPGR", (req,res) => {
-  const sql = `SELECT * FROM DPGR`;
-  connection.query(sql, (error, results) => {
-    if (error) {
-      console.log("ERROR");
-    }
-    res.send(results);
-  });
-});
-/*
 
 
-
-
-// app.get("/Connexion" , (req,res)=>{
-//     res.render('Connexion.ejs');
-// })
-
-
-// app.post('/Connexion',encoder,(req,res)=>{
-//   var email = req.body.email; 
-//   var password = req.body.password; 
-//   connection.query("SELECT * FROM DPGR where email = ? and password = ?" ,[email,password],function(error,results,fields){
-//     if(results.length>0){
-//         res.redirect("/Welcome"); 
-//     } else{
-//         res.redirect("/Connexion"); 
+// app.get("/DPGR", (req,res) => {
+//   const sql = `SELECT * FROM DPGR`;
+//   connection.query(sql, (error, results) => {
+//     if (error) {
+//       console.log("ERROR");
 //     }
-//     res.end(); 
-//   })
-// })
+//     res.send(results);
+//   });
+// });
+
 
 
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/Connexion', async (req, res) => {
-  try {
-    await login(req, res);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({ message: 'Server error' });
-  }
-});
 
+app.get("/connexion" , (req,res)=>{
+  res.send("The connexion"); 
+})
 
-
-
-
-
-*/
-
-app.post("/RegisterDoctorant", (req, res) => {
-  const { Nom, Prenom, Id_Doctorant, Id_PV, Numero_tel, Email, Birthday, Sex } =
-    req.body;
-  // TODO: Validate user input and save to database
-  res.send("Registration successful!");
-});
-
-/*
-
-
-
-
-
-
-
-*/
-
-app.post("/Radiation", (req, res) => {
-  const ids = req.body.ids;
-  const PV_Id = req.body.pv_id;
-
-  console.log("Received IDs:", ids, PV_Id);
-
-  connection.query(
-    "UPDATE Doctorant SET statut = ?, radie = ? WHERE Id_Doctorant IN (?) AND soutenu = NULL and radie = NULL",
-    ["radié", PV_Id, ids],
-    (error, results, fields) => {
-      if (error) {
-        console.log("Error while radiation du doctorant:", error);
-        res.sendStatus(500);
-      } else {
-        console.log("Doctorant radié successfully");
-        res.sendStatus(200);
-      }
+app.post('/connexion', (req,res)=>{
+  var email = req.body.email; 
+  var password = req.body.password; 
+  connection.query("SELECT * FROM DPGR where email = ? and password = ?" ,[email,password],function(error,results,fields){
+    if(results.length>0){
+        res.send(results); 
+        console.log("There is a match"); 
+    } else{
+        res.status(401).send("Invalid email or password");
+        console.log("No match"); 
     }
-  );
-});
-
-/*
+  })
+})
 
 
 
 
-
-
-
-*/
-
-app.post("/Soutenance", (req, res) => {
-  const ids = req.body.ids;
-  const PV_Id = req.body.pv_id;
-
-  console.log("Received IDs:", ids, PV_Id);
-
-  connection.query(
-    "UPDATE Doctorant SET statut = ?, soutenu = ? WHERE Id_Doctorant IN (?) AND soutenu IS NULL AND radie IS NULL",
-    ["soutenu", PV_Id, ids],
-    (error, results, fields) => {
-      if (error) {
-        console.log("Error updating la soutenance du doctorant:", error);
-        res.sendStatus(500);
-      } else {
-        console.log("Doctorant soutenance updated successfully");
-        res.sendStatus(200);
-      }
-    }
-  );
-});
-
-/*
+// app.post('/Connexion', async (req, res) => {
+//   try {
+//     await login(req, res);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send({ message: 'Server error' });
+//   }
+// });
 
 
 
@@ -215,74 +141,151 @@ app.post("/Soutenance", (req, res) => {
 
 
 
-*/
+// app.post("/RegisterDoctorant", (req, res) => {
+//   const { Nom, Prenom, Id_Doctorant, Id_PV, Numero_tel, Email, Birthday, Sex } =
+//     req.body;
+//   // TODO: Validate user input and save to database
+//   res.send("Registration successful!");
+// });
 
-app.post("/Reinscription", (req, res) => {
-  const ids = req.body.ids;
-  const PV_Id = req.body.pv_id;
+// /*
 
-  console.log("Received IDs:", ids, PV_Id);
 
-  //1ST : all the doctorants that are not radié ou soutenu : their statute will be set to differe
-  connection.query(
-    "UPDATE Doctorant SET statut = ? WHERE soutenu IS NULL AND radie IS NULL",
-    ["différé"],
-    (error, results, fields) => {
-      if (error) {
-        console.log("Error putting statut to differe:", error);
-        res.sendStatus(500);
-      } else {
-        console.log("statut updated to differe succesfully successfully");
-        res.sendStatus(200);
-      }
-    }
-  );
 
-  //2ND : update the selected doctorants statut to inscrit
-  connection.query(
-    "UPDATE Doctorant SET statut = ? WHERE Id_Doctorant IN (?) AND soutenu IS NULL AND radie IS NULL",
-    ["inscrit", ids],
-    (error, results, fields) => {
-      if (error) {
-        console.log("Error putting statut to inscrit:", error);
-        res.sendStatus(500);
-      } else {
-        console.log("statut updated to inscrit succesfully successfully");
-        res.sendStatus(200);
-      }
-    }
-  );
 
-  //3RD : increment the number of registration years
-  connection.query(
-    "UPDATE Doctorant SET Nombre_inscriptions = Nombre_inscriptions + 1 WHERE Id_Doctorant IN (?)",
-    [ids],
-    (error, results, fields) => {
-      if (error) {
-        console.log("Error incrememnting number of inscription years:", error);
-        res.sendStatus(500);
-      } else {
-        console.log("registration years incremented successfully");
-        res.sendStatus(200);
-      }
-    }
-  );
 
-  //4TH : add all doctorants registered with the ID to the table Inscription
-  const values = ids.map((id) => [PV_Id, id]);
-  const query = "INSERT INTO Inscription (Id_PV, Id_Doctorant) VALUES (?, ?)";
 
-  connection.query(query, values, (error, results, fields) => {
-    if (error) {
-      console.log("Error inserting all inscriptions:", error);
-      res.sendStatus(500);
-    } else {
-      console.log("inscriptions registered successfully");
-      res.sendStatus(200);
-    }
-  });
 
-  //5TH : add the PV if it doesn't exist : the Id and the date for now and let the link empty
-});
+// */
+
+// app.post("/Radiation", (req, res) => {
+//   const ids = req.body.ids;
+//   const PV_Id = req.body.pv_id;
+
+//   console.log("Received IDs:", ids, PV_Id);
+
+//   connection.query(
+//     "UPDATE Doctorant SET statut = ?, radie = ? WHERE Id_Doctorant IN (?) AND soutenu = NULL and radie = NULL",
+//     ["radié", PV_Id, ids],
+//     (error, results, fields) => {
+//       if (error) {
+//         console.log("Error while radiation du doctorant:", error);
+//         res.sendStatus(500);
+//       } else {
+//         console.log("Doctorant radié successfully");
+//         res.sendStatus(200);
+//       }
+//     }
+//   );
+// });
+
+// /*
+
+
+
+
+
+
+
+// */
+
+// app.post("/Soutenance", (req, res) => {
+//   const ids = req.body.ids;
+//   const PV_Id = req.body.pv_id;
+
+//   console.log("Received IDs:", ids, PV_Id);
+
+//   connection.query(
+//     "UPDATE Doctorant SET statut = ?, soutenu = ? WHERE Id_Doctorant IN (?) AND soutenu IS NULL AND radie IS NULL",
+//     ["soutenu", PV_Id, ids],
+//     (error, results, fields) => {
+//       if (error) {
+//         console.log("Error updating la soutenance du doctorant:", error);
+//         res.sendStatus(500);
+//       } else {
+//         console.log("Doctorant soutenance updated successfully");
+//         res.sendStatus(200);
+//       }
+//     }
+//   );
+// });
+
+// /*
+
+
+
+
+
+
+
+// */
+
+// app.post("/Reinscription", (req, res) => {
+//   const ids = req.body.ids;
+//   const PV_Id = req.body.pv_id;
+
+//   console.log("Received IDs:", ids, PV_Id);
+
+//   //1ST : all the doctorants that are not radié ou soutenu : their statute will be set to differe
+//   connection.query(
+//     "UPDATE Doctorant SET statut = ? WHERE soutenu IS NULL AND radie IS NULL",
+//     ["différé"],
+//     (error, results, fields) => {
+//       if (error) {
+//         console.log("Error putting statut to differe:", error);
+//         res.sendStatus(500);
+//       } else {
+//         console.log("statut updated to differe succesfully successfully");
+//         res.sendStatus(200);
+//       }
+//     }
+//   );
+
+//   //2ND : update the selected doctorants statut to inscrit
+//   connection.query(
+//     "UPDATE Doctorant SET statut = ? WHERE Id_Doctorant IN (?) AND soutenu IS NULL AND radie IS NULL",
+//     ["inscrit", ids],
+//     (error, results, fields) => {
+//       if (error) {
+//         console.log("Error putting statut to inscrit:", error);
+//         res.sendStatus(500);
+//       } else {
+//         console.log("statut updated to inscrit succesfully successfully");
+//         res.sendStatus(200);
+//       }
+//     }
+//   );
+
+//   //3RD : increment the number of registration years
+//   connection.query(
+//     "UPDATE Doctorant SET Nombre_inscriptions = Nombre_inscriptions + 1 WHERE Id_Doctorant IN (?)",
+//     [ids],
+//     (error, results, fields) => {
+//       if (error) {
+//         console.log("Error incrememnting number of inscription years:", error);
+//         res.sendStatus(500);
+//       } else {
+//         console.log("registration years incremented successfully");
+//         res.sendStatus(200);
+//       }
+//     }
+//   );
+
+//   //4TH : add all doctorants registered with the ID to the table Inscription
+//   const values = ids.map((id) => [PV_Id, id]);
+//   const query = "INSERT INTO Inscription (Id_PV, Id_Doctorant) VALUES (?, ?)";
+
+//   connection.query(query, values, (error, results, fields) => {
+//     if (error) {
+//       console.log("Error inserting all inscriptions:", error);
+//       res.sendStatus(500);
+//     } else {
+//       console.log("inscriptions registered successfully");
+//       res.sendStatus(200);
+//     }
+//   });
+
+//   //5TH : add the PV if it doesn't exist : the Id and the date for now and let the link empty
+// });
 
 app.listen(PORT, console.log(`SERVER IS RUNNING ON PORT ${PORT}`));
