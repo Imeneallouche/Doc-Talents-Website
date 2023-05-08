ebd4365781122a371f55d836a33242b03d48c5e5
Oussama Abderraouf Attia <92870205+OussamaAbderraoufAttia@users.noreply.github.com>
2023-05-08 19:14:09 +0100

    Update Statistics.jsx to the newer version!

--- Client/src/pages/Statistics.jsx
+++ Client/src/pages/Statistics.jsx
@@ -1,14 +1,27 @@
 import React from "react";
-import ProfileIcon from "../components/ProfileIcon/ProfileIcon";
 import SideBar from "../components/Menu/SideBar";
+import ProfileIcon from "../components/ProfileIcon/ProfileIcon";
+import StatsPageDataFetcher from "../Data/StatsPageDataFetcher";
+import CriteriaBar from "../components/Statistics/CriteriaBar";
+import { BarChart } from "../components/Statistics/BarChart";
+
+
 
 function Statistics() {
+  //Getting the data from the server using the DataFetcher
+
   return (
     <div className={`bg-white-bluish w-full flex `}>
       <SideBar />
-      <div className="grow flex flex-col justify-between">
+      <div className={`grow flex flex-col justify-start`}>
         <div className="flex justify-end">
           <ProfileIcon />
+        </div>
+        <div className="flex justify-center">
+          <CriteriaBar />
+        </ div >  
+        <div>
+        <BarChart />
         </div>
       </div>
     </div>
