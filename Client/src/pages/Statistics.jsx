acbc598b31d1b29ec4c857822336da8d455677c2
Oussama Abderraouf Attia <92870205+OussamaAbderraoufAttia@users.noreply.github.com>
2023-05-09 06:59:43 +0100

    Update Statistics.jsx for imene

--- Client/src/pages/Statistics.jsx
+++ Client/src/pages/Statistics.jsx
@@ -4,24 +4,34 @@ import ProfileIcon from "../components/ProfileIcon/ProfileIcon";
 import StatsPageDataFetcher from "../Data/StatsPageDataFetcher";
 import CriteriaBar from "../components/Statistics/CriteriaBar";
 import { BarChart } from "../components/Statistics/BarChart";
-
+import ProgressBarChart from "../components/Statistics/ProgressBarChart";
 
 
 function Statistics() {
-  //Getting the data from the server using the DataFetcher
+  // Getting the data from the server using the DataFetcher
 
   return (
-    <div className={`bg-white-bluish w-full flex `}>
-      <SideBar />
-      <div className={`grow flex flex-col justify-start`}>
+    <div className="bg-white-bluish w-full flex">
+      <div style={{ position: "fixed", top: 0, bottom: 0 }}>
+        {/* Sidebar component */}
+        <SideBar />
+      </div>
+      <div className="flex flex-col justify-start ml-64">
+        {/* Profile icon */}
         <div className="flex justify-end">
           <ProfileIcon />
         </div>
-        <div className="flex justify-center">
+        {/* Criteria bar */}
+        <div className="flex justify-center ml-20">
           <CriteriaBar />
-        </ div >  
+        </div>
+        {/* Bar chart */}
+        <div className="mt-10">
+          <BarChart />
+        </div>
+        {/* Progress bar */}
         <div>
-        <BarChart />
+          <ProgressBarChart />
         </div>
       </div>
     </div>
