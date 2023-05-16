3fdf98c147afcbf34accd71be16520b929da7928
Oussama Abderraouf Attia <92870205+OussamaAbderraoufAttia@users.noreply.github.com>
2023-05-16 22:49:33 +0100

    Update StatisticsController.js to the perfect version

--- Server/app/Controllers/StatisticsController.js
+++ Server/app/Controllers/StatisticsController.js
@@ -9,25 +9,17 @@ const currentYear = new Date().getFullYear();
 
 
 const StatisticsController = (req, res) => {
-    const { critere1, critere2, première_année, dernière_année } = {
-        critere1: "Femme",
-        critere2: "aucun",
-        première_année: 2011,
-        dernière_année: 2023
-      };
-      
-      console.log("the params are: ", critere1, critere2, première_année, dernière_année);
-      
-      if (dernière_année === "") {
-        dernière_année = currentYear;
+  const { critere1, critere2, premiere_annee, derniere_annee } = req.query;
+      if (derniere_annee === "") {
+        derniere_annee = currentYear;
       }
     
       let reqsql = "";
-      if (dernière_année === "") { dernière_année = currentYear }
+      if (derniere_annee === "") { derniere_annee = currentYear }
         if ((critere1 === "Femme") && (critere2 === "aucun")) { reqsql = "SELECT * FROM Doctorant WHERE sexe='F'AND (Nombre_inscriptions + Premiere_inscription) <= ? AND Premiere_inscription >= ?" }
         if ((critere1 === "Homme") && (critere2 === "aucun")) { reqsql = "SELECT * FROM Doctorant WHERE sexe='M'AND (Nombre_inscriptions + Premiere_inscription) <= ? AND Premiere_inscription >= ?" }
         if ((critere1 === "soutenu") && (critere2 === "aucun")) { reqsql = "SELECT * FROM Doctorant WHERE soutenu IS NOT NULL  AND (Nombre_inscriptions + Premiere_inscription) <= ? AND Premiere_inscription >= ?" }
-        if ((critere1 === "radié") && (critere2 === "aucun")) { reqsql = "SELECT * FROM Doctorant WHERE radie IS NOT NULL AND (Nombre_inscriptions + Premiere_inscription) <= ? AND Premiere_inscription >= ?" }
+        if ((critere1 === "radie") && (critere2 === "aucun")) { reqsql = "SELECT * FROM Doctorant WHERE radie IS NOT NULL AND (Nombre_inscriptions + Premiere_inscription) <= ? AND Premiere_inscription >= ?" }
         if ((critere1 === "inf5") && (critere2 === "aucun")) { reqsql = "SELECT * FROM Doctorant WHERE Nombre_inscriptions<5 AND (Nombre_inscriptions + Premiere_inscription) <= ? AND Premiere_inscription >= ?" }
         if ((critere1 === "entre5_10") && (critere2 === "aucun")) { reqsql = "SELECT * FROM Doctorant WHERE 5<Nombre_inscriptions AND Nombre_inscriptions<10 AND (Nombre_inscriptions + Premiere_inscription) <= ? AND Premiere_inscription >= ?" }
         if ((critere1 === "sup10") && (critere2 === "aucun")) { reqsql = "SELECT * FROM Doctorant WHERE Nombre_inscriptions>10 AND (Nombre_inscriptions + Premiere_inscription) <= ? AND Premiere_inscription >= ?" }
@@ -38,10 +30,10 @@ const StatisticsController = (req, res) => {
             reqsql = "SELECT * FROM Doctorant WHERE sexe='M' AND soutenu IS NOT NULL AND (Nombre_inscriptions + Premiere_inscription) <= ? AND Premiere_inscription >= ?";
           }
     
-          if (((critere1 === "Femme") && (critere2 === "radié")) || ((critere1 === "radié") && (critere2 === "Femme"))) {
+          if (((critere1 === "Femme") && (critere2 === "radie")) || ((critere1 === "radie") && (critere2 === "Femme"))) {
             reqsql = "SELECT * FROM Doctorant WHERE sexe='F' AND radie IS NOT NULL AND (Nombre_inscriptions + Premiere_inscription) <= ? AND Premiere_inscription >= ?";
           }
-          if (((critere1 === "Homme") && (critere2 === "radié")) || ((critere1 === "radié") && (critere2 === "Homme"))) {
+          if (((critere1 === "Homme") && (critere2 === "radie")) || ((critere1 === "radie") && (critere2 === "Homme"))) {
             reqsql = "SELECT * FROM Doctorant WHERE sexe='M' AND radie IS NOT NULL AND (Nombre_inscriptions + Premiere_inscription) <= ? AND Premiere_inscription >= ?";
           }
           if (((critere1 === "Femme") && (critere2 === "inscrit")) || ((critere1 === "inscrit") && (critere2 === "Femme"))) {
@@ -74,7 +66,7 @@ const StatisticsController = (req, res) => {
          if (((critere1 === "Homme") && (critere2 === "sup10")) || ((critere1 === "sup10") && (critere2 === "Homme"))) {
             reqsql = "SELECT * FROM Doctorant WHERE sexe='M' AND Nombre_inscriptions > 10 AND (Nombre_inscriptions + Premiere_inscription) <= ? AND Premiere_inscription >= ?";
          }
-         if (((critere1 === "radié") && (critere2 === "sup10")) || ((critere1 === "sup10") && (critere2 === "radié"))) {
+         if (((critere1 === "radie") && (critere2 === "sup10")) || ((critere1 === "sup10") && (critere2 === "radie"))) {
           reqsql = "SELECT * FROM Doctorant WHERE radie IS NOT NULL AND Nombre_inscriptions > 10 AND (Nombre_inscriptions + Premiere_inscription) <= ? AND Premiere_inscription >= ?";
         }
         if (((critere1 === "soutenu") && (critere2 === "sup10")) || ((critere1 === "sup10") && (critere2 === "soutenu"))) {
@@ -86,7 +78,7 @@ const StatisticsController = (req, res) => {
         if (((critere1 === "différé") && (critere2 === "sup10")) || ((critere1 === "sup10") && (critere2 === "différé"))) {
           reqsql = "SELECT * FROM Doctorant WHERE statut ='différé' AND Nombre_inscriptions > 10 AND (Nombre_inscriptions + Premiere_inscription) <= ? AND Premiere_inscription >= ?";
         }
-        if (((critere1 === "radié") && (critere2 === "entre5_10")) || ((critere1 === "entre5_10") && (critere2 === "radié"))) {
+        if (((critere1 === "radie") && (critere2 === "entre5_10")) || ((critere1 === "entre5_10") && (critere2 === "radie"))) {
           reqsql = "SELECT * FROM Doctorant WHERE radie IS NOT NULL AND Nombre_inscriptions >= 5 AND Nombre_inscriptions <= 10 AND (Nombre_inscriptions + Premiere_inscription) <= ? AND Premiere_inscription >= ?";
        }
        if (((critere1 === "soutenu") && (critere2 === "entre5_10")) || ((critere1 === "entre5_10") && (critere2 === "soutenu"))) {
@@ -98,7 +90,7 @@ const StatisticsController = (req, res) => {
      if (((critere1 === "différé") && (critere2 === "entre5_10")) || ((critere1 === "entre5_10") && (critere2 === "différé"))) {
         reqsql = "SELECT * FROM Doctorant WHERE statut ='différé' AND Nombre_inscriptions >= 5 AND Nombre_inscriptions <= 10 AND (Nombre_inscriptions + Premiere_inscription) <= ? AND Premiere_inscription >= ?";
      }
-       if (((critere1 === "radié") && (critere2 === "inf5")) || ((critere1 === "inf5") && (critere2 === "radié"))) {
+       if (((critere1 === "radie") && (critere2 === "inf5")) || ((critere1 === "inf5") && (critere2 === "radie"))) {
         reqsql = "SELECT * FROM Doctorant WHERE radie IS NOT NULL AND Nombre_inscriptions < 5 AND (Nombre_inscriptions + Premiere_inscription) <= ? AND Premiere_inscription >= ?";
       }
       if (((critere1 === "soutenu") && (critere2 === "inf5")) || ((critere1 === "inf5") && (critere2 === "soutenu"))) {
@@ -114,27 +106,24 @@ const StatisticsController = (req, res) => {
       if ((critere1 === "Femme") && (critere2 === "aucun")) {
         reqsql = "SELECT * FROM Doctorant WHERE sexe='F' AND (Nombre_inscriptions + Premiere_inscription) <= ? AND Premiere_inscription >= ?";
       }
-      // Add other conditions for different combinations of critere1 and critere2
-    
-      db.query(reqsql, [dernière_année, première_année], (error, results) => {
+      
+      db.query(reqsql, [derniere_annee, premiere_annee], (error, results) => {
         if (error) {
           console.log(error);
           res.status(500).send('Error generating statistiques');
         } else {
-          console.log("The results are:");
-          console.log(results);
           nbDoctorants = results.length;
           let maxi = 0;
           let mini = results.length;
-          moyenne = results.length / (dernière_année - première_année + 1);
+          moyenne = results.length / (derniere_annee - premiere_annee + 1);
           
-          for (let i = première_année; i <= dernière_année; i++) {
+          for (let i = premiere_annee; i <= derniere_annee; i++) {
             let cpt = 0;
             for (let j = 0; j < nbDoctorants; j++) {
               if ((results[j].Premiere_inscription <= i) && ((results[j].Premiere_inscription + results[j].Nombre_inscriptions) >= i)) {
                 cpt++;
               }
-              list[i - première_année] = cpt;
+              list[i - premiere_annee] = cpt;
               if (cpt > maxi) {
                 maxi = cpt;
                 max = i;
@@ -146,7 +135,6 @@ const StatisticsController = (req, res) => {
             }
           }
           
-          console.log(nbDoctorants, moyenne, max, min, list);
           res.send({ nbDoctorants, moyenne, max, min, list });
         }
       });
